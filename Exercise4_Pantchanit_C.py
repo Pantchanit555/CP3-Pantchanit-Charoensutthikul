7b24f04d944720e301ff4f28bb7ed81f001b2a8d
Pantchanit555 <50312606+Pantchanit555@users.noreply.github.com>
2019-05-21 16:07:01 +0700

    Add files via upload

---
+++ Exercise4_Pantchanit_C.py
@@ -0,0 +1,9 @@
+fe = 84
+gb = 88
+ic = 90
+cp = 96
+print("--- Your Score ---")
+print("Foundation English : ",fe)
+print("General Business : ",gb)
+print("Introduction to Computer Systems : ",ic)
+print("Computer Programming : ",cp)
