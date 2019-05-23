5e2324ea4f48cfff15e64d8c817b3edbba64dd4f
Pantchanit555 <50312606+Pantchanit555@users.noreply.github.com>
2019-05-23 18:43:53 +0700

    Add files via upload

--- Exercise8_Pantchanit_C.py
+++ Exercise8_Pantchanit_C.py
@@ -12,32 +12,38 @@ if username1 == "store1" and password1 == "2436" :
     if  select1 == 1:
              q = int(input("Quantity : "))
              sumselect = q*35
-             print("Total : ",sumselect)
+             vat = 7
+             print("Total : ",sumselect+sumselect*(vat/100))
              print("--------------------THANK YOU--------------------")
     elif select1 == 2:
             q = int(input("Quantity : "))
             sumselect = q *30
-            print("Total : ", sumselect)
+            vat = 7
+            print("Total : ", sumselect + sumselect * (vat / 100))
             print("--------------------THANK YOU--------------------")
     elif select1 == 3:
             q = int(input("Quantity : "))
             sumselect = q*35
-            print("Total : ", sumselect)
+            vat = 7
+            print("Total : ", sumselect + sumselect * (vat / 100))
             print("--------------------THANK YOU--------------------")
     elif select1 == 4:
             q = int(input("Quantity : "))
             sumselect = q*25
-            print("Total : ", sumselect)
+            vat = 7
+            print("Total : ", sumselect + sumselect * (vat / 100))
             print("--------------------THANK YOU--------------------")
     elif select1 == 5:
             q = int(input("Quantity : "))
             sumselect = q*20
-            print("Total : ", sumselect)
+            vat = 7
+            print("Total : ", sumselect + sumselect * (vat / 100))
             print("--------------------THANK YOU--------------------")
     elif select1 == 6:
             q = int(input("Quantity : "))
             sumselect = q*40
-            print("Total : ", sumselect)
+            vat = 7
+            print("Total : ", sumselect + sumselect * (vat / 100))
             print("--------------------THANK YOU--------------------")
 else :
     print("!!!!!!!!!!!!!!!!!!!!!!WRONG!!!!!!!!!!!!!!!!!!!!!!!")
