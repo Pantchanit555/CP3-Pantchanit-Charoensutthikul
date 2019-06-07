372824b791371823809a546195e79ae5c0d1980a
Pantchanit555 <50312606+Pantchanit555@users.noreply.github.com>
2019-06-07 15:24:49 +0700

    Add files via upload

---
+++ Lecture_50_Pantchanit_C.py
@@ -0,0 +1,14 @@
+def  addnumber(x,y) :
+    print("x+y =",x+y)
+def  deletenumber(x,y) :
+    print("x-y =",x-y)
+def  multiplynumber(x,y) :
+    print("x*y =",x*y)
+def  devidenumber(x,y) :
+    print("x/y =",x/y)
+x = int(input("x = "))
+y = int(input("y = "))
+addnumber(x,y)
+deletenumber(x,y)
+multiplynumber(x,y)
+devidenumber(x,y)
